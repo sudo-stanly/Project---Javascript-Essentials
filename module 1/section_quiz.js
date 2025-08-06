4432222f566e27eec7fc777e019b31f48ce7556a
stanly-sudo <alys.git24@gmail.com>
2025-08-06 11:54:26 +0800

    section quiz

---
+++ module 1/section_quiz.js
@@ -0,0 +1,26 @@
+
+// Q1 Use console.log to output your full name to the console.
+console.log("Stanley Alerta");
+
+// Q2 Output your year of birth.
+console.log("2004");
+
+// Q3 Try again to output your year of birth, this time passing the date without the quotes.
+console.log(2004);
+
+// Q4 We can pass several arguments to console.log separated by commas, e.g.:
+console.log("abc", "def", "ghi", 1, 2, 3);
+
+    // Output information about yourself to the console in the format: Name Surname (Year) e.g. Mary Stuart (1542).
+    console.log("Stanley", "Alerta", "2004");
+
+// Q5 Output the same information (name, surname, year) to the console, not side by side, but on consecutive lines.
+console.log("Stanley");
+console.log("Alerta");
+console.log(2004);
+
+// Q6  A string can be concatenated using the + sign, for example "abc" + "def" will be treated as "abcdef". Try writing your name, surname, and year of birth on one line again, this time not separated by commas, but by + signs.
+console.log("Stanley"+"Alerta"+"2004");
+
+// Question 7: Put spaces in the appropriate places, so that when displayed, you get the same effect as in Question 4.
+console.log("Stanley "+"Alerta "+"2004");
