721276b7217146e2ca1d9ca8e8e1de1943d87537
stanly-sudo <alys.git24@gmail.com>
2025-08-08 06:22:08 +0800

    module 2 - section 1

--- module 2/section 1/lab_variables.js
+++ module 2/section 1/lab_variables.js
@@ -16,7 +16,25 @@ Scenario
 Our task will be to create a list of contacts. 
 Initially, the list will be quite simple: we will only 
 write three people to it using the data shown in the table below. 
+
 In the rest of the course, you will return to this script and systematically 
 extend it with new functionality, using the newly learned elements of JavaScript.
 
+
+Declare and initialize the variables where you 
+will store all the information (nine variables in total). 
+Display in the console information about the first and last 
+contact in the form: name/phone/email.
 */ 
+
+const studentName1 = "Maxwell Wright";
+const studentContact1 = "(0191) 719 6495";
+const studentEmail1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+const studentName2 = "Raja Villarreal";
+const studentContact2 = "0866 398 2895";
+const studentEmail2 = "posuere.vulputate@sed.com";
+
+const studentName3 = "Helen Richards";
+const studentContact3 = "0800 1111";
+const studentEmail3 = "libero@convallis.edu";
